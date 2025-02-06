9ef2a2e2e3bb19f27ba8e1e03c390a7fdda60f6a
ikomers <komers.for@gmail.com>
2025-02-06 21:42:01 +0300

    Initial commit

---
+++ src/main/java/com/example/insurance_spark_hadoop/InsuranceSparkHadoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurance_spark_hadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceSparkHadoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceSparkHadoopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/insurance_spark_hadoop/InsuranceSparkHadoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurance_spark_hadoop;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceSparkHadoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
